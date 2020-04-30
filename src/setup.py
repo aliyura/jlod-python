490342b1db394241a908252e87f34885adbd5259
rabs-developer <developer.rabs@gmail.com>
2020-04-30 14:08:55 +0100

    Preparing Setup

--- src/setup.py
+++ src/setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="jlod_db",
+    name="jlod",
     version="0.0.1",
-    author="Rabiu Aliyu [ Rabs ]",
-    author_email="Net.RabiuAliyu@Gmail.com",
-    description="A small local database package",
-    long_description=long_description,
+    author="JLOD.ORG",
+    author_email="contact@jload.org",
+    description="Self Contained Document Oriented Database",
+    long_description="JLOD stands for JSON Local Document Database, it is a serverless in-process library that implements a self-contained document-oriented database use for storing small application data within an application without an internet connection. The database is like MongoDB, it uses Collection, Documents, and JSON objects.  The JLOD is a version of SQLite in object-oriented format, the JLOD collections can be exported to Remote MongoDB collection as well as remote MongoDB collection can also be imported to the JLOD database. JLOD is an embedded document-oriented database library. Unlike MongoDB.  JLOD does not have a separate server process. JLOD reads and writes the data to ordinary disk files.  The complete JLOD database along with the collections and the documents are contained in a disk file. The folder is a database in JLOD while the file is a collection, and each line in the file is a document.",
     long_description_content_type="text/markdown",
     url="https://github.com/rabs-developer/jlod_db",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: MTN Approved :: TT License",
+        "License :: MIT License",
         "Operating System :: OS Independent",
     ],
     install_requires=[
